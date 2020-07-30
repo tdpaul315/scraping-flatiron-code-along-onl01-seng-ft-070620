48f5190fd31af2742d6fcf4f013436ce0e716f69
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-30 10:21:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,40 +1,27 @@
 require 'nokogiri'
 require 'open-uri'
-require 'pry'
-
+ 
 require_relative './course.rb'
-
+ 
 class Scraper
-  
-  def get_page 
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    
-    # doc.css(".post").each do |post|
-    #   course = Course.new
-    #   course.title = post.css("h2").text
-    #   course.schedule = post.css(".date").text
-    #   course.description = post.css("p").text
-<<<<<<< HEAD
-  end 
-
-def get_courses
-  self.get_page.css(".post")
-end
-
-def make_courses
-  self.get_courses.each do |post|
-    course = Course.new
-    course.title = post.css("h2").text
-    course.schedule = post.css(".date").text
-    course.description = post.css("p").text
+ 
+  def get_page
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
-end
-=======
-    
+ 
+  def get_courses
+    self.get_page.css(".post")
   end
-end 
->>>>>>> 0437dba39f2db7bdcf88dcfb65bbc8a03f25f7d7
-  
+ 
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+ 
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -45,13 +32,7 @@ end
       end
     end
   end
-  
+ 
 end
-
-<<<<<<< HEAD
-Scraper.new.print_courses 
-=======
-Scraper.new.get_page 
->>>>>>> 0437dba39f2db7bdcf88dcfb65bbc8a03f25f7d7
-
-
+ 
+Scraper.new.print_courses
